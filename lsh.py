ed255308896fe13088d31743fc375e4250f56d81
ThomasChatzopoulos <chatzothomas@gmail.com>
2021-01-21 15:09:57 +0200

    create bands - minor fixes - comments

--- lsh.py
+++ lsh.py
@@ -7,11 +7,15 @@ import os
 import nltk
 from nltk.corpus import stopwords
 
-
 # get documents directory
 
 
-
+# read documents from directory,
+# preprocessing on text (words to lowercase, removing punctuation marks, split, remove stopwords)
+# create shingles
+# return:
+# 1. zip_list: list of which each row contains hashed shingle and the names of the documents in which it appears
+# 2. docs: the names of all documents
 def create_shingles(MYDIR):
     k = 5
     docs = [""]
@@ -19,11 +23,11 @@ def create_shingles(MYDIR):
     shingles = []
     hashed_shingles = []
     path_of_docs = MYDIR + '/sample/'
-    print("Preproccesing...")
+    print("Preprocessing...")
     for root, dirs, files in os.walk(path_of_docs, topdown=False):
         # for every file
         for name in files:
-            print("name", name)
+            print("name:", name)
             doc = os.path.join(root, name)
             # read file
             file = open(doc, "r", encoding="UTF-8", errors='ignore')
@@ -42,7 +46,7 @@ def create_shingles(MYDIR):
                 # create singles
                 shingle = filteredContents[i:i + k]
                 # create hashed shingle
-                hashed = binascii.crc32(shingle.encode('utf8')) & 0xffffffff  # --------- 32bit ---------
+                hashed = binascii.crc32(shingle.encode('utf8')) & 0xffffffff  # 32bit
                 if hashed not in hashed_shingles:
                     # store unique hashed shingles
                     hashed_shingles.append(hashed)
@@ -62,7 +66,11 @@ def create_shingles(MYDIR):
         print("zip_list: ", zip_list[r])
     return zip_list, docs
 
-
+# Given the zip_list and docs list it creates the input matrix (sparse matrix).
+# input_matrix: rows: hashed shingles
+#               columns: documents names
+#               cells: 1 --> hashed shingles of row r is a member of the document of column c
+#                      0 --> hashed shingles of row r is not a member of the document of column c
 def create_input_matrix(zip_list, docs):
     input_matrix = []
     # store document names in first row
@@ -86,7 +94,8 @@ def create_input_matrix(zip_list, docs):
     return input_matrix
 
 
-def next_prime(value):  # find next prime number ### def next_prime(value):
+# find next prime number of a number
+def next_prime(value):
     for num in range(value, value*value):  # for num in range(value,2**33): ν παίρνεις την επόμενη δύναμη
         if all(num % i != 0 for i in range(2, int(math.sqrt(num)) + 1)):
             return num
@@ -110,17 +119,16 @@ def randomCoefficients(functions_no):
     return randomNumbers
 
 
-def minHash(input_matrix, docs):
-    # number of hash functions
-    hash_no = 10
+# Creates the signature matrix given the input matrix and the number of hash functions
+def minHash(input_matrix, docs, hash_num):
     # max shingle value
     max_shingle_value = 2 ** 32 - 1
     p = next_prime(max_shingle_value + 1)  # p = next_prime(max_shingle_value)
-    a_coef = randomCoefficients(hash_no)
-    b_coef = randomCoefficients(hash_no)
+    a_coef = randomCoefficients(hash_num)
+    b_coef = randomCoefficients(hash_num)
     signature_matrix = []
     # initialize signature matrix
-    for j in range(hash_no):
+    for j in range(hash_num):
        signature_matrix.append([p + 1] * (len(docs) - 1))
     print("signature_matrix: ", signature_matrix)
 
@@ -128,7 +136,7 @@ def minHash(input_matrix, docs):
     for row in range(1, len(input_matrix)):
         # create hash functions for this row
         hash_func = []
-        for i in range(hash_no):
+        for i in range(hash_num):
             hashed_function = (a_coef[i] * row + b_coef[i]) % p
             hash_func.append(hashed_function)
         print("hash_func: ", hash_func)
@@ -139,7 +147,7 @@ def minHash(input_matrix, docs):
             # print("input_matrix[row][column]: ", input_matrix[row][column+1])
             if input_matrix[row][column + 1] == 1:
                 # for each i= 1, 2, ..., n
-                for hf in range(hash_no):
+                for hf in range(hash_num):
                     # if hash_function(hf) is smaller than sig_matrix(row,column)
                     if hash_func[hf] < signature_matrix[hf][column]:
                         # set sig_matrix(row,column) to hash_function(hf)
@@ -147,13 +155,57 @@ def minHash(input_matrix, docs):
     return signature_matrix
 
 
+# Finds the set of table size divisors and
+# assigns appropriate values to the variables r and b for for the number of bands and rows
+def get_b_r(length_of_sign_matrix):
+    factors = []
+    for i in range(1, length_of_sign_matrix + 1):
+        if length_of_sign_matrix % i == 0:
+            factors.append(i)
+    if len(factors) > 3:
+        r = factors[int(len(factors) / 2) - 1]
+    elif len(factors) == 3:
+        r = factors[int(len(factors) / 2)]
+    else:
+        print("Give number of functions diferent from", length_of_sign_matrix)
+        return
+    b = int(length_of_sign_matrix / r)
+    return b, r
+
+
+# creates bands given the signature matrix
+def create_bands(signature_matrix):
+    bands = []
+    b, r = get_b_r(len(signature_matrix))
+    print("\n#bands = ", b, "#rows = ", r)
+
+    for band in range(b):
+        rows = []
+        for row in range(r):
+            rows.append(signature_matrix[band + row])
+        bands.append([band, rows])
+    return bands
+
+
 def main():
     MYDIR = dirname(__file__)  # gives back your directory path
+    hash_num = 10
+
     hashed_shingles, docs = create_shingles(MYDIR)
     inp_mtrx = create_input_matrix(hashed_shingles, docs)
-    sm = minHash(inp_mtrx, docs)
-    for r in range(len(sm)):
-        print("minHash: ", r, sm[r])
+    sign_mtrx = minHash(inp_mtrx, docs, hash_num)
+    for r in range(len(sign_mtrx)):
+        print("minHash:", r, "-->", sign_mtrx[r])
+    bands = create_bands(sign_mtrx)
+    print("bands: ")
+    for r in range(len(bands)):
+        print(r, "-->", end='')
+        for s_r in range(len(bands[r][1])):
+            if s_r == 0:
+                print(bands[r][1][s_r])
+            else:
+                print("\t", bands[r][1][s_r])
+
 
     #print(sm)
 
