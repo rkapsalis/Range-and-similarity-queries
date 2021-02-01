95f382b1a52f2e30ed88f2607fd0d951fbc3172e
ThomasChatzopoulos <chatzothomas@gmail.com>
2021-02-01 15:01:23 +0200

    Add comments, minor fixes (document_similarities() and cand_pairs)

--- lsh.py
+++ lsh.py
@@ -1,9 +1,6 @@
 import math
 import binascii
 import random
-import re
-import os
-from nltk.corpus import stopwords
 
 
 def create_shingles(sim_docs, k):
@@ -19,7 +16,7 @@ def create_shingles(sim_docs, k):
     shingles = []
     hashed_shingles = []
 
-    print("Preprocessing...")
+    print("\nCalculations...")
 
     # for every file
     for name in sim_docs:
@@ -49,13 +46,12 @@ def create_shingles(sim_docs, k):
                 if doc_name not in zip_list[position[0][0]][1]:
                     zip_list[position[0][0]][1].append(doc_name)
 
-    for r in range(len(zip_list)):
-        print("zip_list: ", zip_list[r])
+    # for r in range(len(zip_list)):
+    #     print("zip_list: ", zip_list[r])
     return zip_list, docs
 
 
 def create_input_matrix(zip_list, docs):
-
     # Given the zip_list and docs list it creates the input matrix (sparse matrix).
     # input_matrix: rows: hashed shingles
     #               columns: documents names
@@ -79,13 +75,14 @@ def create_input_matrix(zip_list, docs):
         # store row in input matrix
         input_matrix.append(row)
 
-    for r in range(len(input_matrix)):
-        print(input_matrix[r])
+    # for r in range(len(input_matrix)):
+    #     print(input_matrix[r])
     return input_matrix
 
 
 def next_prime(value):
     # Returns next prime number after a given number
+
     for num in range(value, value * value):
         # square root method to find if a number is prime or not
         if all(num % i != 0 for i in range(2, int(math.sqrt(num)) + 1)):
@@ -94,14 +91,17 @@ def next_prime(value):
 
 def randomCoefficients(functions_no):
     # Generates random numbers a,b for every hash function
+
     randomNumbers = []
     functions_counter = 0
     max_shingle_value = 2 ** 32 - 1
     # for every hash function
     while functions_counter < functions_no:
         # generate random number in range (0, max_shingle_value)
+        # random.seed(0)
         random_number = random.randint(0, max_shingle_value)
         # we want the number to be unique.
+        # random.seed(0)
         while random_number in randomNumbers:
             # if the number is not unique, create new random number
             random_number = random.randint(0, max_shingle_value)
@@ -129,7 +129,6 @@ def minHash(input_matrix, docs, hash_num):
     # for every hash function
     for j in range(hash_num):
         signature_matrix.append([p + 1] * (len(docs) - 1))
-    print("signature_matrix: ", signature_matrix)
 
     # for every row of the input matrix (every hashed single)
     for row in range(1, len(input_matrix)):
@@ -139,7 +138,7 @@ def minHash(input_matrix, docs, hash_num):
             # universal hashing
             hashed_function = (a_coef[i] * row + b_coef[i]) % p
             hash_func.append(hashed_function)
-        print("hash_func: ", hash_func)
+        # print("hash_func: ", hash_func)
 
         # for every column of the input matrix
         for column in range(len(docs) - 1):
@@ -160,15 +159,23 @@ def get_b_r(length_of_sign_matrix):
 
     factors = []
     for i in range(1, length_of_sign_matrix + 1):
+        # if i exactly divides the length_of_sign_matrix
         if length_of_sign_matrix % i == 0:
             factors.append(i)
+
+    # if the number of divisors is greater than 3
     if len(factors) > 3:
+        # number of rows (r) takes the first lower than the mid value
         r = factors[int(len(factors) / 2) - 1]
+    # if the number of divisors is 3
     elif len(factors) == 3:
+        # number of rows (r) takes the the mid value
         r = factors[int(len(factors) / 2)]
     else:
+        # if the number of divisors is less than 3 (inappropriate number for bands)
         print("Give number of functions different from", length_of_sign_matrix)
         return
+    # must be length_of_sign_matrix = b*r
     b = int(length_of_sign_matrix / r)
     return b, r
 
@@ -179,16 +186,18 @@ def create_bands(signature_matrix):
     # to b bands each of which consists of r rows (b*r=n)
 
     bands = []
+    # get the number of bands and the number of rows
     b, r = get_b_r(len(signature_matrix))
-    print("\n#bands = ", b, "#rows = ", r)
-
     row_to_add = 0
+
+    # for each one of b bands
     for band in range(b):
         rows = []
         for row in range(r):
-            # print("band,row", band, row)
+            # get r rows from signature_matrix
             rows.append(signature_matrix[row_to_add])
             row_to_add = row_to_add + 1
+        # add r rows to this band
         bands.append([band, rows])
     return bands
 
@@ -209,13 +218,14 @@ def create_hash_table(bands, k):
         buckets.append(row)
 
     # hash function parameters
+    # random.seed(0)
     a = random.randint(1, 99)
+    # random.seed(0)
     b = random.randint(1, 99)
     for band in range(len(bands)):
         for column in range(len(bands[band][1][0])):
             sum = 0
             for band_len in range(len(bands[band][1])):
-                # print(bands[band][1][band_len][column], end=', ')
                 sum = sum + bands[band][1][band_len][column]
             position_in_hashed = ((a * sum + b) % 99) % k
             buckets[band][position_in_hashed].append(column)
@@ -229,36 +239,55 @@ def candidate_column_pairs(buckets):
     # returns only the candidate pairs of documents (zip array of buckets)
 
     candidate_pairs_list = []
+    # for each bucket array (for row of buckets)
     for row in range(len(buckets)):
         row_candidates_matches = []
+        # for each bucket in bucket array
         for column in range(len(buckets[row])):
+            # if bucket contains 2 or more documents
             if len(buckets[row][column]) > 1:
-                column_candidates_matches = []
-                for matches in range(len(buckets[row][column])):
-                    column_candidates_matches.append(buckets[row][column][matches])
-                row_candidates_matches.append(column_candidates_matches)
+                # add bucket to new zip bucket array
+                row_candidates_matches.append(buckets[row][column])
+        # add zip bucket array to candidate_pairs_list
         candidate_pairs_list.append(row_candidates_matches)
     return candidate_pairs_list
 
 
 def jaccard_similarity(list1, list2):
     # jaccard_similarity function calculates the Jaccard similarity of 2 lists
+
     s1 = set(list1)
     s2 = set(list2)
-    return float(len(s1.intersection(s2)) / len(s1.union(s2)))
+    return float(str(round(len(s1.intersection(s2)) / len(s1.union(s2)), 3)))
 
 
 def document_similarities(cand_pairs, sign_mtrx, docs):
     # document_similarities function calculates the Jaccard similarity of all pairs
+
     similarities = []
-    for bucket in range(len(cand_pairs)):
-        for row in range(len(cand_pairs[bucket])):
-            for l in range(len(cand_pairs[bucket][row])):
-                for s_l in range(l + 1, len(cand_pairs[bucket][row])):
-                    first = cand_pairs[bucket][row][l]
-                    second = cand_pairs[bucket][row][s_l]
-                    list_1 = [i[first] for i in sign_mtrx]
-                    list_2 = [i[second] for i in sign_mtrx]
-                    js = jaccard_similarity(list_1, list_2)
-                    similarities.append([docs[first + 1], docs[second + 1], js])
+    # for each bucket array in candidate pairs list
+    for bucket_array in range(len(cand_pairs)):
+        # for each bucket in bucket array
+        for bucket in range(len(cand_pairs[bucket_array])):
+            # find all pairs from a list (if list size > 2)
+            # A,B,C,D --> AB,AC,AD - BC,BD - CD
+            for doc_1 in range(len(cand_pairs[bucket_array][bucket])):
+                for doc_2 in range(doc_1 + 1, len(cand_pairs[bucket_array][bucket])):
+                    first = cand_pairs[bucket_array][bucket][doc_1]
+                    second = cand_pairs[bucket_array][bucket][doc_2]
+                    # if documents are different from each other
+                    if first != second:
+                        # get signatures of documents from signature matrix
+                        list_1 = [s[first] for s in sign_mtrx]
+                        list_2 = [s[second] for s in sign_mtrx]
+                        # calculate jaccard similarity
+                        js = jaccard_similarity(list_1, list_2)
+                        # if record / pair does not exist in similarities add it
+                        if ([docs[first + 1], docs[second + 1], js] or [docs[second + 1], docs[
+                        first + 1], js]) not in similarities:
+                            similarities.append([docs[first + 1], docs[second + 1], js])
+
+    # sort results
+    similarities = sorted(similarities, key=lambda s: s[2], reverse=True)
+
     return similarities
